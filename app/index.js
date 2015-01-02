d5f7845e3d8cd88b0b5f90e56cfddf8a0df65834
Scott Nesham <sanesham@gmail.com>
2015-01-02 01:02:56 -0600

    file ops

--- app/index.js
+++ app/index.js
@@ -3,6 +3,21 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
+var baseNodeRequirements = [
+  'connect-livereload',
+  'load-grunt-tasks',
+  'grunt',
+  'grunt-contrib-connect',
+  'grunt-contrib-jade',
+  'grunt-contrib-watch',
+  'juice',
+  'grunt-juice-email',
+  'grunt-open'
+];
+var baseBowerRequirements = [
+  'ink'
+];
+
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
     this.pkg = require('../package.json');
@@ -21,44 +36,77 @@ module.exports = yeoman.generators.Base.extend({
       name: 'campaign',
       message: 'What should we call this email campaign?',
       default: this.appName
-    },{
-
-    }];
+    },
+    // later give people the option to turn off preprocessing and templates
+    // {
+    //   type: 'checkbox',
+    //   name: 'baseOptions',
+    //   message: 'Please check the features you would like to use',
+    //   choices: [
+    //     'a templating language',
+    //     'css preprocessing',
+    //     'zurb ink base stylesheet'
+    //   ]
+    // }
+    {
+      type: 'list',
+      name: 'preprocessor',
+      message: 'What preprocessor would you like to use?',
+      choices: [
+        'sass',
+        'less'
+      ],
+      default: 'sass'
+    }
+    ];
 
     this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+      this.campaign     = props.campaign;
+      this.preprocessor = props.preprocessor;
 
       done();
     }.bind(this));
   },
 
-  writing: {
-    app: function () {
+  folderScaffolding: {
+    app: function() {
       this.fs.copy(
         this.templatePath('_package.json'),
         this.destinationPath('package.json')
       );
+
       this.fs.copy(
         this.templatePath('_bower.json'),
         this.destinationPath('bower.json')
       );
-    },
 
-    projectfiles: function () {
       this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
+        this.templatePath('src/*'),
+        this.destinationPath('src/*')
       );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
+    },
+  },
+
+  setup: function () {
+    //bower
+    this.bowerInstall(baseBowerRequirements, {'saveDev': true});
+
+    //npm
+    this.npmInstall(baseNodeRequirements, { 'saveDev': true });
+
+    //install chosen precompiler
+    if (this.preprocessor === 'sass') {
+      this.npmInstall(['grunt-contrib-sass'], { 'saveDev': true });
+    }else if( this.preprocessor === 'less'){
+      this.npmInstall(['grunt-contrib-less'], { 'saveDev': true });
     }
   },
 
-  install: function () {
-    this.installDependencies({
-      skipInstall: this.options['skip-install']
-    });
+
+  inkConfig: function () {
+    this.fs.copy(
+      this.destinationRoot('/bower_components/ink/ink.css'),
+      this.destinationRoot('')
+    );
   }
 });
